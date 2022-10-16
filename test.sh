9455704f5f242e31ae91c28d3e47ba0691da9581
9glenda <plan9git@proton.me>
2022-10-16 18:03:41 +0000

    added teststests and a few improvements to the assembler

--- test.sh
+++ test.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
+set -eou pipefail
 tests=0
-failed=0
+passed=0
 test() {
   tests=$((tests+1))
-  lc3ash_time="$(./lc3ash "tests/$1/main.asm")"
-  lc3as_time="$(lc3as "tests/$1/main.asm")"
+  ./lc3ash "tests/$1/main.asm"
+  lc3as "tests/$1/main.asm"  2>/dev/null 1>&2
   file1="$(hexdump -C "tests/$1/main.asm.out")"
   file2="$(hexdump -C "tests/$1/main.obj")"
-  if [ ! "$file1" == "$file2" ]; then
+  if [ ! "$file1" = "$file2" ]; then
     printf 'TEST FAILED %s\nlc3ash:\n%s\nlc3as:\n%s' "$1" "$file1" "$file2"
-    failed=$((failed+1))
+    # failed=$((failed+1))
   else
     printf 'TEST PASSED %s\n' "$1" # "$lc3ash_time" "$lc3as_time"
+    passed=$((passed+1))
   fi
   rm "tests/$1/main.asm.out" "tests/$1/main.obj" "tests/$1/main.sym"
 }
-shellcheck lc3ash
+shellcheck lc3ash test.sh
 test halt
 test orig_decimal
 test fill
 test helloworld
 test 2labels
 test 3labels
+test end
 test lea
 test ld
 test ldi
@@ -29,4 +32,5 @@ test st
 test sti
 test jmp
 test big_file
-printf 'TESTS FAILED: (%s/%s)' "$failed" "$tests"
+printf 'TESTS PASSED: (%s/%s)' "$passed" "$tests"
+exit $((tests-passed))
---
+++ tests/end/main.asm
@@ -0,0 +1,7 @@
+.orig x3000
+	lea r0, string
+	puts
+	halt
+	string .stringz "hello world"
+.end
+halt
